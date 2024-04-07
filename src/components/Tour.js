9adebb0bc09b4bd952d947d604bac083598738ab
Mehul-Gandhi <mgandhi1127@gmail.com>
2024-04-06 20:31:26 -0700

    Add map

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,13 +1,41 @@
-import { useParams } from 'react-router-dom';
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import AppContext from '../AppContext'; // Adjust the path as needed
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
-export default function Tour () {
-    let { tripId } = useParams();
-    const navigate = useNavigate();
-    const { routesData } = useContext(AppContext);
-    const trip = routesData[parseInt(tripId) - 1];
-    console.log(trip);
-    return (<div>{trip.name}</div>);
-}
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+const center = {
+  lat: 7.2905715, // default latitude
+  lng: 80.6337262, // default longitude
+};
+
+const Tour = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: '',
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={10}
+        center={center}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default Tour;
